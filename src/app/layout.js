83447f2b00d01fef7b18e3de25ba9b27d1a4f9ae
devmashum <dev.mashum@gmail.com>
2023-12-06 17:58:12 +0100

    footer section done

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from '@/components/shared/Navbar'
 import Footer from '@/components/shared/Footer'
+import { Container } from '@mui/material'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
       <NavBar></NavBar>
-      {children}</body>
+     <Container className='min-h-screen'> {children}</Container>
+      </body>
       <Footer></Footer>
     </html>
   )
--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,4 +1,4 @@
-import { Box, Button, Container, IconButton, Typography } from '@mui/material';
+import { Box, Button, Container, IconButton, Stack, Typography } from '@mui/material';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import React from 'react';
 import Link from 'next/link';
@@ -32,32 +32,36 @@ const navItems   = [
 },
 ];
     return (
-        <Box className="bg-black">
-     <Container>    
-     <Box className="text-center">
-     <IconButton>
-
-     <FacebookIcon/>
-           </IconButton>
+        <Box className="bg-black px-2 py-10">
+     <Container>
+     <Box className="w-full text-center" sx={{
+            "& svg":{
+                color:"white"
+                },
+         }}>
            <IconButton>
-               <FacebookIcon/>
-           </IconButton>
-           <IconButton>
-               <FacebookIcon/>
-           </IconButton>
-           </Box>
-           <Box className="w-full text-center ">
+                <FacebookIcon/>
+            </IconButton>
+            <IconButton>
+                <FacebookIcon/>
+            </IconButton>
+            <IconButton>
+                <FacebookIcon/>
+            </IconButton>
+          </Box>
+          <Box className="w-full text-center">
             {navItems.map((item) => (
             <Link key={item} href={item.pathname}>
 
-                <Button className="text-white mx-3 ">
+                <button className="text-white mx-3">
                     {item.route}
-                </Button>
+                </button>
             </Link>
             ))}
           </Box>
-<Typography className='text-center'>@ 2024 The Dragon News</Typography>
-          </Container>
+      <Typography variant='body2' color="gray" textAlign={"center"} className='mt-3'>@ 2023 The Dragon News. Designed by Faisal</Typography>
+     </Container>    
+  
          </Box>
     );
 };
